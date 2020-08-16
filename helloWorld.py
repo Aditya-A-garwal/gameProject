f5be6fe5fccb354b70fb08647cc3bdf15b1d2165
Aditya-A-garwal <aditya.agarwal030504@gmail.com>
2020-08-16 11:47:54 +0530

    Aditya Agarwal: Added temporary game icon, tested simple rendering of image

--- helloWorld.py
+++ helloWorld.py
@@ -1,11 +1,22 @@
 import pygame
-
+pygame.init()
 print("Hello world")
 
 screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Terraria")
+pygame.display.set_icon(pygame.image.load("Assets/imgtester.png"))
 
-running=True
+img = pygame.image.load("Assets/imgtester.png")
+
+def renderTester():
+    xCoor = 100
+    yCoor = 100
+    screen.blit(img, (xCoor, yCoor), (16, 0, 16, 16))
+
+running = True
 while running:
+    screen.fill((255, 255, 255))
+    renderTester()
     for event in pygame.event.get():
-        if event.type in (pygame.QUIT, pygame.KEYDOWN):
-            running=False
+        if event.type in (pygame.QUIT, pygame.KEYDOWN): running = False
+    pygame.display.update()
