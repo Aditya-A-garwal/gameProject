f3e0794b19bc050517b9cbc65403e5666ac9cb7d
Aditya-A-garwal <aditya.agarwal030504@gmail.com>
2020-08-20 20:44:10 +0530

    Aditya Agarwal: Added docstrings to Translator, made changes to comment lines of DB

--- Translator.py
+++ Translator.py
@@ -20,16 +20,32 @@ Translations
 
 # Translate coordinates from array-space to chunk-space
 def arrayToChunk(coor):
+    """Translates given coordinates from array to in-chunk
+
+    Requires coordinates as a sequence of length 2
+    """
     return [coor[0]*16, coor[1]*16]
 
 # Translate coordinates from chunk-space to absolute-space
-def chunkToGraph(coor, chunkNumber):
-    return [coor[0]+(chunkNumber*8), coor[1]]
+def chunkToGraph(coor, chunkIndex):
+    """Translates given coordinates from in-chunk to world
+
+    Requires the coordinates as a sequence and the index of the chunk as an int
+    """
+    return [coor[0]+(chunkIndex*8), coor[1]]
 
 # Translate coordinates from absolute-space to camera-space
 def graphToCamera(coor, cameraCoor):
+    """Translates given coordinates from world to relative to camera
+
+    Requires the coordinates as a sequence and the coordinates of the camera as a sequence
+    """
     return [coor[0]-cameraCoor[0], coor[1]-cameraCoor[1]]
 
 # Translate coordinates from camera-space to screen-space
 def cameraToScreen(coor, displaySize):
+    """Translates given coordinates from relative to camera to screen
+
+    Requires the coordinates as a sequence and the dimensions of the display area as a sequence
+    """
     return [coor[0], displaySize[1]-coor[1]]
