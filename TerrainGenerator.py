6e966fbc0ae05119c9b386c8aeb228c50b04f094
Vikas Jain <vikajain@adobe.com>
2020-08-22 22:15:51 +0530

    Akshaj: modified TerrainGenerator

--- TerrainGenerator.py
+++ TerrainGenerator.py
@@ -6,7 +6,7 @@ import random
 walkingConstant = 0.02*random.random()*2.5
 
 def populateChunk(chunk, noiseObj, chunkInd):
-    coor = chunkInd * chunkWidth + random.randrange(5,15)
+    coor = chunkInd * chunkWidth
     print(walkingConstant, coor)
     for i in range(0, chunkWidth):
         height = int((noiseObj.noise2d(x=coor*walkingConstant, y=(random.randint(0,2)/75))+1)*32)  # Value will be from 0 to 64
