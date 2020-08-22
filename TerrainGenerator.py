8aa01e8c8699afae16baf5770c51ac1ff0eeeab0
Vikas Jain <vikajain@adobe.com>
2020-08-22 21:45:15 +0530

    Akshaj: modified TerrainGenerator

--- TerrainGenerator.py
+++ TerrainGenerator.py
@@ -2,12 +2,13 @@ import pygame
 from Chunk import *
 from Tile import *
 from opensimplex import OpenSimplex
-
-walkingConstant = 0.05
+import random
+walkingConstant = 0.02*random.random()*2.5
 
 def populateChunk(chunk, noiseObj, chunkInd):
-    coor = chunkInd * chunkWidth
+    coor = chunkInd * chunkWidth + random.randrange(5,15)
+    print(walkingConstant, coor)
     for i in range(0, chunkWidth):
-        height = int((noiseObj.noise2d(x=coor*walkingConstant, y=0)+1)*32)  # Value will be from 0 to 64
+        height = int((noiseObj.noise2d(x=coor*walkingConstant, y=(random.randint(0,2)/75))+1)*32)  # Value will be from 0 to 64
         coor += 1
         for j in range(96, 95+height): chunk[j,i] = Grass()
