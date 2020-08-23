db1401fc3a508479626a951d97f426084473f996
Vikas Jain <vikajain@adobe.com>
2020-08-23 10:15:29 +0530

    Akshaj: modified DBIO

--- databaseIO.py
+++ databaseIO.py
@@ -1,55 +1,56 @@
 import sqlite3
 
-# Initialization (To be run only once)
-try:
-    conn = sqlite3.connect('world.db')
-    c = conn.cursor()
+class DBIO:
+    conn = 'a'
+    def __init__(self, name):
+        # Initialization (To be run only once)
+        name = name + '.db'
+        try:
+            self.conn = sqlite3.connect(name)
+            c = self.conn.cursor()
 
-    # Create Table
-    c.execute('''CREATE TABLE terrain(keys INTEGER NOT NULL PRIMARY KEY, binry TEXT)''')
-    conn.commit()
-    conn.close()
-except:
-    pass
+            # Create Table
+            c.execute('''CREATE TABLE terrain(keys INTEGER NOT NULL PRIMARY KEY, binry TEXT)''')
+            self.conn.commit()
+        except:
+            pass
 
-# Save/Update function
-def save(key, binary):
+    # Save/Update function
+    def save(self, key, binary):
+        """Saves/Updates the string at a particular key location.
 
-    """Saves/Updates the string at a particular key location.
+        Requires the key as an int and the UTF-8 string.
+        """
+        c = self.conn.cursor()
+        try:    # Save string at new key location
+            c.execute('''INSERT INTO terrain VALUES (?,?)''', (key, binary))
+            conn.commit()
 
-    Requires the key as an int and the UTF-8 string.
-    """
-    conn = sqlite3.connect('world.db')  # Connect to the DB (if not present then create a new DB)
-    c = conn.cursor()
-    try:    # Save string at new key location
-        c.execute('''INSERT INTO terrain VALUES (?,?)''', (key, binary))
-        conn.commit()
+        except: # Update string at existing key
+            c.execute('UPDATE terrain SET binry =?  WHERE keys=?', (binary, key))
+            self.conn.commit()
 
-    except: # Update string at existing key
-        c.execute('UPDATE terrain SET binry =?  WHERE keys=?', (binary, key))
-        conn.commit()
-    conn.close()
+    # Load function
+    def load(self, key):
+        """Retrieves the string stored at a particular key location.
 
-# Load function
-def load(key):
-    """Retrieves the string stored at a particular key location.
+        Requires the key as an int.
+        Returns the string at the key's location (if key is present) or None
+        """
 
-    Requires the key as an int.
-    Returns the string at the key's location (if key is present) or None
-    """
-    conn = sqlite3.connect('world.db')  # Connect to DB (if not present then create a new DB)
-
-    c = conn.cursor()
-    c.execute('''SELECT binry FROM terrain WHERE keys=?''', (key,))
-    res = c.fetchone()
-    conn.commit()
-    conn.close()
-    try:
-        return res[0]
-    except:
-        return res
+        c = self.conn.cursor()
+        c.execute('''SELECT binry FROM terrain WHERE keys=?''', (key,))
+        res = c.fetchone()
+        self.conn.commit()
+        try:
+            return res[0]
+        except:
+            return res
+    def stop(self):
+        self.conn.close()
 
 # Testing
-for i in range(-3,4,1): save(i, b'\x80\x04\x95\x0b\x00\x00\x00\x00\x00\x00\x00]\x94(K\x01K\x02K\x03e.')
-
-for i in range(-1,5): print(retrieve(i))
+a = DBIO('world')
+a.save(-188888, 'hi')
+print(a.load(-188888))
+a.stop()
