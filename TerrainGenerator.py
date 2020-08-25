955b50a788456c92230838d7fb98d057a4df0118
Aditya-A-garwal <aditya.agarwal030504@gmail.com>
2020-08-25 11:27:02 +0530

    Aditya: Fixed import bug

--- TerrainGenerator.py
+++ TerrainGenerator.py
@@ -29,5 +29,47 @@ def loadChunks(chunkBuffer, positionBuffer, chunkInd, persistence, noiseObj):
         positionBuffer[posInBuff] = i
         posInBuff+=1
 
+class ChunkBuffer:
+
+    def init(self, length, storageObj, chunkInd):
+
+        self.storage = storageObj
+        self.currChunk = chunkInd
+
+        self.chunks = []
+        self.positions = []
+
+        if(length % 2 != 0):
+
+            for i in range(int(self.currChunk-(length-1)*0.5), int(self.currChunk+1+(length-1)*0.5)):
+                self.positions.append(i)
+                tempObj = self.storage[i]
+                if(tempObj == None):
+                    tempObj = Chunk()
+                    populateChunk(tempObj)
+                self.chunks.append(tempObj)
+
+
+    def shiftLeft(self):
+        for i in range(0, len(self.positions)): self.positions[i] += 1
+        self.storage[self.positions[0]] = self.chunks[0]
+        for i in range(0, len(self.chunks)-1):
+            self.chunks[i] = self.chunks[i+1]
+        self.chunks[-1] = self.storage[self.positions[-1]]
+
+    def shiftRight(self):
+        for i in range(0, len(self.positions)): self.positions[i] -= 1
+
+        self.storage[self.positions[-1]] = self.chunks[-1]
+        for i in range(len(self.chunks), 0, -1):
+            self.chunks[i] = self.chunks[i-1]
+        self.chunks[0] = self.storage[self.positions[0]]
+
+    def ___getitem__(self, key):
+        return self.chunks[key]
+
+    def __setitem__(self, key, value):
+        self.chunks[key] = value
+
 
 
